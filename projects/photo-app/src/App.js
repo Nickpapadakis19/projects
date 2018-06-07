b85c259a8ba593ec9b02902ce3bbc0a3f684db98
nickamadeus <papadakn@gmatc.matc.edu>
2018-06-07 15:42:56 -0500

    got the star rating system working!! woot. Probably could be done a little cleaner, but it works great

--- projects/photo-app/src/App.js
+++ projects/photo-app/src/App.js
@@ -14,13 +14,79 @@ constructor(props){
     user : {
       userID : '',
       password : '',
-      date : '',
+      date : new Date(),
       rating : ''
+
+    },
+    class : {
+      class_one : 'fa fa-star star',
+      class_two : 'fa fa-star star',
+      class_three : 'fa fa-star star',
+      class_four : 'fa fa-star star',
+      class_five : 'fa fa-star star'
     }
   }
+this.userRating = this.userRating.bind(this);
+this.userInfo = this.userInfo.bind(this);
+}
+
+userInfo = () => {
 
 }
 
+userRating = (event) => {
+    let userId = event.target.id;
+    let state = this.state
+    switch(userId){
+
+      case 'one':
+        state.user.rating = 'One Star';
+        state.class.class_one = 'fa fa-star star checked';
+        state.class.class_two = 'fa fa-star star';
+        state.class.class_three = 'fa fa-star star';
+        state.class.class_four = 'fa fa-star star';
+        state.class.class_five = 'fa fa-star star';
+        this.setState = ({state});
+        break;
+      case 'two':
+        state.user.rating = 'Two Stars';
+        state.class.class_one = 'fa fa-star star checked';
+        state.class.class_two = 'fa fa-star star checked';
+        state.class.class_three = 'fa fa-star star';
+        state.class.class_four = 'fa fa-star star';
+        state.class.class_five = 'fa fa-star star';
+        this.setState = ({state});
+        break;
+      case 'three':
+        state.user.rating = 'Three Stars';
+        state.class.class_one = 'fa fa-star star checked';
+        state.class.class_two = 'fa fa-star star checked';
+        state.class.class_three = 'fa fa-star star checked';
+        state.class.class_four = 'fa fa-star star';
+        state.class.class_five = 'fa fa-star star';
+        this.setState = ({state});
+        break;
+      case 'four':
+        state.user.rating = 'Four Stars';
+        state.class.class_one = 'fa fa-star star checked';
+        state.class.class_two = 'fa fa-star star checked';
+        state.class.class_three = 'fa fa-star star checked';
+        state.class.class_four = 'fa fa-star star checked';
+        state.class.class_five = 'fa fa-star star';
+        this.setState = ({state});
+        break;
+      case 'five':
+        state.user.rating = 'Five Stars';
+        state.class.class_one = 'fa fa-star star checked';
+        state.class.class_two = 'fa fa-star star checked';
+        state.class.class_three = 'fa fa-star star checked';
+        state.class.class_four = 'fa fa-star star checked';
+        state.class.class_five = 'fa fa-star star checked';
+        this.setState = ({state});
+        break;
+
+    }
+  }
 
   render() {
     return (
@@ -29,7 +95,9 @@ constructor(props){
         <Header />
         <Route exact={true} path="/" component={Home} />
         <Route path="/SignIn" component={SignIn} />
-        <Route path="/Photo" component={Photo} />
+        <Route path="/Photo" render={() => (
+        <Photo rate={this.userRating} class={this.state.class} />
+        )} />
       </div>
     </BrowserRouter>
     );
--- projects/photo-app/src/Screens/Photo.js
+++ projects/photo-app/src/Screens/Photo.js
@@ -7,53 +7,25 @@ class Photo extends Component {
 constructor(props){
   super(props)
 
-this.state = {
-  rating: {
-    stars : '',
-    date : new Date()
-  },
-  class : true
 }
-this.userRating = this.userRating.bind(this);
-}
-
-userRating = (event) => {
-    let userId = event.target.id;
-
-    switch(userId){
-      case 'one':
-        this.setState = ({ stars : 'One Stars',
-        class : false })
-        break;
-      case 'two':
-        this.setState = ({ stars : 'Two Stars'})
-        break;
-      case 'three':
-        this.setState = ({stars : 'Three Stars'})
-        break;
-      case 'four':
-        this.setState = ({stars : 'Four Stars'})
-        break;
-      case 'five':
-        this.setState = ({stars : 'Five Stars'})
-        break;
-    }
-  }
-
-
 
   render(){
-  let btn_class = this.state.class ? "fa fa-star star" : "fa fa-star star checked";
+
     return(
       <div id="photo-div">
         <img id="photo" src={photo}  alt="Landscape"/>
         <div id="star-div">
           <h3>Rate Today's Photo</h3>
-          <button id="one" className={btn_class} onClick={this.userRating}></button>
-          <button id="two" className="fa fa-star star" onClick={this.userRating}></button>
-          <button id="three" className="fa fa-star star" onClick={this.userRating}></button>
-          <button id="four" className="fa fa-star star" onClick={this.userRating}></button>
-          <button id="five" className="fa fa-star star" onClick={this.userRating}></button>
+          <Link to="/Photo"><button id="one" className={this.props.class.class_one} onClick={this.props.rate}></button></Link>
+          <Link to="/Photo"><button id="two" className={this.props.class.class_two} onClick={this.props.rate}></button></Link>
+          <Link to="/Photo"><button id="three" className={this.props.class.class_three} onClick={this.props.rate}></button></Link>
+          <Link to="/Photo"><button id="four" className={this.props.class.class_four} onClick={this.props.rate}></button></Link>
+          <Link to="/Photo"><button id="five" className={this.props.class.class_five} onClick={this.props.rate}></button></Link>
+        </div>
+        <div>
+          <button id="photo-rate">Rate</button>
+          <Link to="/"><button id="photo-cancel">Cancel</button></Link>
+
         </div>
       </div>
 
